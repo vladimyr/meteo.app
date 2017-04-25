579d57adbe8d5e891dacadf18930cef08e0b3c9e
Dario Vladovic <d.vladimyr@gmail.com>
2017-04-25 02:20:13 +0200

    [WIP] Swapping cors proxy
    - migrating to cors-proxy.now.sh
    - reformatting code using editorconfig

--- index.js
+++ index.js
@@ -4,41 +4,41 @@ const $ = require('cash-dom');
 const r = require('qwest');
 const urlJoin = require('url-join');
 
-const proxy = 'https://cors-anywhere.herokuapp.com/';
 const baseUrl = 'http://prognoza.hr/';
 
 function fetchReports(location) {
-    let reportUrl = urlJoin(proxy, baseUrl, `/tri/${ location }.xml`)
-    return r.get(reportUrl, null, { responseType: 'xml' })
-        .then((_, xml) => parseReports(xml));
+  let reportUrl = urlJoin(baseUrl, `/tri/${ location }.xml`);
+  let url = `https://cors-proxy.now.sh/?url=${reportUrl}`;
+  return r.get(url, null, { responseType: 'xml' })
+    .then((_, xml) => parseReports(xml));
 }
 
 function parseReports(xmldoc) {
-    let $root = $(xmldoc);
-    let data = $root.find('dan').map(day => {
-        let $day = $(day);
-        let date = $day.attr('datum');
-        let forecasts = $day.find('doba_dana').map(data => parseForecast(data));
-        return { date, forecasts };
-    });
-    return data;
+  let $root = $(xmldoc);
+  let data = $root.find('dan').map(day => {
+    let $day = $(day);
+    let date = $day.attr('datum');
+    let forecasts = $day.find('doba_dana').map(data => parseForecast(data));
+    return { date, forecasts };
+  });
+  return data;
 }
 
 function symbol(name, suffix='.gif') {
-    return { name, url: `${ baseUrl }/alasimboli/${ name }${ suffix }` };
+  return { name, url: `${ baseUrl }/alasimboli/${ name }${ suffix }` };
 }
 
 function parseForecast(data) {
-    let $data = $(data);
-    return {
-        conditions: symbol($data.find('simbol').text(), 's.gif'),
-        wind: symbol($data.find('vjetar').text()),
-        temperature: {
-            max: parseInt($data.find('tmax').text(), 10),
-            min: parseInt($data.find('tmin').text(), 10)
-        }
-    };
+  let $data = $(data);
+  return {
+    conditions: symbol($data.find('simbol').text(), 's.gif'),
+    wind: symbol($data.find('vjetar').text()),
+    temperature: {
+      max: parseInt($data.find('tmax').text(), 10),
+      min: parseInt($data.find('tmin').text(), 10)
+    }
+  };
 }
 
 fetchReports('Trogir')
-    .then(reports => console.log(JSON.stringify(reports, null, 2)));
+  .then(reports => console.log(JSON.stringify(reports, null, 2)));
